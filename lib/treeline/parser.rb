0cdd7bdd2e6d01438aaa046050dd28445d251fb3
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-05-10 17:29:29 -0500

    Bulk out Treeline::Parser.parse(), and add unit tests to match
    Signed-off-by: Craig S. Cottingham <craig.cottingham@gmail.com>

--- lib/treeline/parser.rb
+++ lib/treeline/parser.rb
@@ -2,8 +2,53 @@
 
 module Treeline
   class Parser
+    
     def parse(lines)
-      {}
+      tree = {}
+      @path = []
+      
+      lines.each do | line |
+        # line.chomp!
+        line.rstrip!
+        line = yield(line) if block_given?
+        
+        # first, catch root elements (with no indent)
+        if (line =~ /^\S/)
+          @path = [ [ line, 0 ] ]
+        else
+          # next, catch indented elements
+          matched = /^(\s+)/.match(line)
+          unless (matched.nil?)
+            indent = matched[1].length
+            while (indent <= @path[0][1])
+              @path.shift
+            end
+            line.lstrip!
+            @path.unshift [ line, indent ]
+          end
+        end
+
+        # puts self.to_s
+        make_path_to_node(tree, @path.reverse)
+        
+      end
+      
+      tree
     end
+  
+  protected
+  
+    def make_path_to_node(tree, path)
+      make_path_to_node(tree[path[0][0]], path[1..-1]) if (path.count > 1)
+      tree[path[0][0]] = {} if (path.count == 1)
+      tree
+    end
+    
+  private
+  
+    def to_s
+      @path.reverse.map { | item | item[0] }.join(" > ")
+    end
+    
   end
 end
--- test/01_parser_test.rb
+++ test/01_parser_test.rb
@@ -16,9 +16,33 @@ class ParserTest < Test::Unit::TestCase
     lines << "    A::B3::D2"
     
     parser = Treeline::Parser.new
+
     tree = parser.parse lines
     assert_not_nil tree
+    assert_equal 1, tree.keys.count
+    assert_has_key tree, 'A'
     
+    subtree = tree['A']
+    assert_not_nil subtree
+    assert_equal 3, subtree.keys.count
+    assert_has_key subtree, 'A::B1'
+    assert_has_key subtree, 'A::B2'
+    assert_has_key subtree, 'A::B3'
+    
+    subsubtree = subtree['A::B1']
+    assert_not_nil subsubtree
+    assert_equal 0, subsubtree.keys.count
+    
+    subsubtree = subtree['A::B2']
+    assert_not_nil subsubtree
+    assert_equal 1, subsubtree.keys.count
+    assert_has_key subsubtree, 'A::B2::C'
+    
+    subsubtree = subtree['A::B3']
+    assert_not_nil subsubtree
+    assert_equal 2, subsubtree.keys.count
+    assert_has_key subsubtree, 'A::B3::D1'
+    assert_has_key subsubtree, 'A::B3::D2'
   end
   
   def test_parser_with_block
@@ -32,11 +56,43 @@ class ParserTest < Test::Unit::TestCase
     lines << "    A::B3::D2"
     
     parser = Treeline::Parser.new
+    
     tree = parser.parse(lines) do | item |
       item.downcase
     end
     assert_not_nil tree
+    assert_equal 1, tree.keys.count
+    assert_has_key tree, 'a'
     
+    subtree = tree['a']
+    assert_not_nil subtree
+    assert_equal 3, subtree.keys.count
+    assert_has_key subtree, 'a::b1'
+    assert_has_key subtree, 'a::b2'
+    assert_has_key subtree, 'a::b3'
+    
+    subsubtree = subtree['a::b1']
+    assert_not_nil subsubtree
+    assert_equal 0, subsubtree.keys.count
+    
+    subsubtree = subtree['a::b2']
+    assert_not_nil subsubtree
+    assert_equal 1, subsubtree.keys.count
+    assert_has_key subsubtree, 'a::b2::c'
+    
+    subsubtree = subtree['a::b3']
+    assert_not_nil subsubtree
+    assert_equal 2, subsubtree.keys.count
+    assert_has_key subsubtree, 'a::b3::d1'
+    assert_has_key subsubtree, 'a::b3::d2'
+    
+  end
+
+protected
+
+  def assert_has_key(tree, key, msg=nil)
+    msg = "Key '#{key}' not found" if msg.nil?
+    flunk msg unless tree.has_key?(key)
   end
   
 end
